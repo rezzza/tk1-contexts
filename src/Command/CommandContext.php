rezzza/tk1-contexts

commit b6cfec0200f1670cab926fe2775ae3dfcc98f826

As this step is used for debug, so nice to remember its name ;)

src/Command/CommandContext.php
@@ -127,6 +127,7 @@ class CommandContext extends BehatContext implements KernelAwareInterface
 
     /**
      * @Then /^I should see the last command output$/
+     * @Then /^print last command output$/
      */
     public function iShouldSeeTheLastCommandOutput()
     {

commit 15e147f10064ddc193f9cba57c3dc561a60bbf16

Fix BC break introduced in b6cfec0200f1670cab926fe2775ae3dfcc98f826
Thanks to @Spy-Seth who notice we can use multiple step definition

src/Command/CommandContext.php
@@ -126,6 +126,7 @@ class CommandContext extends BehatContext implements KernelAwareInterface
     }
 
     /**
+     * @Then /^I should see the last command output$/
      * @Then /^print last command output$/
      */
     public function iShouldSeeTheLastCommandOutput()